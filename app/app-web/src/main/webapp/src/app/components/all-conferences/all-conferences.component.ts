731f7b2b0d3a9abdf62e42d39a6a47009e926ac4
unknown <alexaandreas99@gmail.com>
2020-05-22 21:22:33 +0300

    added search option

--- app/app-web/src/main/webapp/src/app/components/all-conferences/all-conferences.component.ts
+++ app/app-web/src/main/webapp/src/app/components/all-conferences/all-conferences.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ConferenceService} from '../../services/conference.service';
 import {Conference} from '../../models/conference.model';
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
@@ -11,30 +11,42 @@ import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 
 export class AllConferencesComponent implements OnInit {
   conferences: Conference[];
-
+  result: Conference[];
   conference = new Conference();
   coChairEmail: string;
-  coChairsEmails = new Array<String>();
+  coChairsEmails = new Array<string>();
+  filterKey: string;
 
   constructor(
     private conferenceService: ConferenceService,
     config: NgbModalConfig,
     private modalService: NgbModal
-  ) { }
+  ) {
+  }
 
 
   ngOnInit(): void {
     this.conferenceService.getConferences().subscribe(conf => {
       this.conferences = conf;
+      this.result = conf;
     });
   }
-  
+
   open(content) {
     this.modalService.open(content);
   }
 
-  saveConference(): void{
+  saveConference(): void {
     console.log(this.conference);
   }
 
+  filter(event) {
+    const filterKey = event.target.value;
+    if (filterKey === '') {
+      this.result = this.conferences;
+    } else {
+      // tslint:disable-next-line:max-line-length
+      this.result = this.conferences.filter(conference => conference.title.search(filterKey) >= 0 || conference.description.search(filterKey) >= 0);
+    }
+  }
 }

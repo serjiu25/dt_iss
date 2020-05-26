2287143f67703b9ba3e521bb294c015c2067f20a
Tudor Badiu <tudor.badiu@gmail.com>
2020-05-26 17:39:35 +0300

    added getConferencesByPC/Author isPC/Author

--- app/app-web/src/main/java/iss/dt/app/web/controller/ConferenceController.java
+++ app/app-web/src/main/java/iss/dt/app/web/controller/ConferenceController.java
@@ -2,7 +2,9 @@ package iss.dt.app.web.controller;
 
 import iss.dt.app.core.model.Conference;
 import iss.dt.app.core.service.ConferenceService;
+import iss.dt.app.core.service.UserService;
 import iss.dt.app.web.converter.ConferenceConverter;
+import iss.dt.app.web.converter.UserConverter;
 import iss.dt.app.web.dto.ConferenceDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +20,10 @@ public class ConferenceController {
     private ConferenceService service;
     @Autowired
     private ConferenceConverter converter;
+    @Autowired
+    private UserConverter userConverter;
+    @Autowired
+    private UserService userService;
 
     //todo: updateConference fields
     @CrossOrigin(origins = "*")
@@ -45,6 +51,36 @@ public class ConferenceController {
         Conference conference = service.findOne(conferenceId);
         //log.trace("getConference: conferences={}", conference);
         return converter.convertModelToDto(conference);
+    }
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/conferences/author/{userId}", method = RequestMethod.GET)
+    public List<ConferenceDto> getConferencesByAuthor(@PathVariable final Long userId) {
+        //log.trace("getConference");
+
+        List<Conference> conferences = service.findForAuthor(userId);
+        //log.trace("getConference: conferences={}", conference);
+        return ArrayList<>(converter.convertModelsToDtos(conferences));
+    }
+    @RequestMapping(value = "/conferences/pc/{userId}", method = RequestMethod.GET)
+    public List<ConferenceDto> getConferencesByPC(@PathVariable final Long userId) {
+        //log.trace("getConference");
+        List<Conference> conferences = service.findForPC(userId);
+        //log.trace("getConference: conferences={}", conference);
+        return ArrayList<>(converter.convertModelsToDtos(conferences));
+    }
+    @RequestMapping(value = "/conferences/isAuthor/{confId}/{userId}", method = RequestMethod.GET)
+    public boolean isAuthor(@PathVariable final Long userId) {
+        //log.trace("getConference");
+        boolean response = service.isAuthor(confId,userId);
+        //log.trace("getConference: conferences={}", conference);
+        return response;
+    }
+    @RequestMapping(value = "/conferences/isPC/{confId}/{userId}", method = RequestMethod.GET)
+    public boolean isPC(@PathVariable final Long userId) {
+        //log.trace("getConference");
+        boolean response = service.isPC(confId,userId);
+        //log.trace("getConference: conferences={}", conference);
+        return response;
     }
 
     @CrossOrigin(origins = "*")

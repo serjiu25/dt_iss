6f7ceb63be0ad0a818a6de21fdce7975b6d54c42
Tudor Alexa <tudor.alexa99@gmail.com>
2020-05-26 19:35:23 +0300

    add paper component

--- app/app-web/src/main/webapp/src/app/components/conference/conference.component.ts
+++ app/app-web/src/main/webapp/src/app/components/conference/conference.component.ts
@@ -3,6 +3,7 @@ import {Conference} from "../../models/conference.model";
 import {ConferenceService} from "../../services/conference.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Paper} from "../../models/paper.model";
+import {PaperService} from "../../services/paper.service";
 
 @Component({
   selector: 'app-conference',
@@ -21,27 +22,31 @@ export class ConferenceComponent implements OnInit {
 
   constructor(
     private conferenceService: ConferenceService,
-    // private paperService: PaperService,
+    private paperService: PaperService,
     private route: ActivatedRoute
   ) {
   }
 
   ngOnInit(): void {
-    this.paperList = new Array<Paper>();
-    this.hardcodedPaper.name = "Title hardcoded";
-    this.hardcodedPaper.abstractUrl = "abstract";
-    this.hardcodedPaper.fullUrl = "full";
-    this.hardcodedPaper.topics = "topics";
-    this.hardcodedPaper.keywords = "keywords";
-    this.hardcodedPaper.otherAuthors = "Other authors";
 
-    this.paperList.push(this.hardcodedPaper);
+    // this.hardcodedPaper.name = "Title hardcoded";
+    // this.hardcodedPaper.abstractUrl = "abstract";
+    // this.hardcodedPaper.fullUrl = "full";
+    // this.hardcodedPaper.topics = "topics";
+    // this.hardcodedPaper.keywords = "keywords";
+    // this.hardcodedPaper.otherAuthors = "Other authors";
+
+    // this.paperList.push(this.hardcodedPaper);
     this.route.paramMap.subscribe(paramMap => {
       this.conferenceId = Number(paramMap.get('id'));
       this.conferenceService.getConference(
         this.conferenceId
       ).subscribe(obj => {
         this.conference = obj;
+        this.paperList = new Array<Paper>();
+        this.paperService.getPapersByCid(this.conference.id).subscribe(papers => {
+          this.paperList = papers;
+        });
       });
     });
   }
--- app/app-web/src/main/webapp/src/app/services/conference.service.ts
+++ app/app-web/src/main/webapp/src/app/services/conference.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {Conference} from '../models/conference.model';
-import { User } from '../models/user.model';
+// import { User } from '../models/user.model';
 import { Phase } from '../models/phase.enum';
 import { environment } from 'src/environments/environment';
 import {User} from '../models/user.model';

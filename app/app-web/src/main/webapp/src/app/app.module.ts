cd1f6e81e7628a58672771e884dec4fd4bd71141
Tudor Alexa <tudor.alexa99@gmail.com>
2020-05-29 19:23:38 +0300

    add bidding process

--- app/app-web/src/main/webapp/src/app/app.module.ts
+++ app/app-web/src/main/webapp/src/app/app.module.ts
@@ -5,21 +5,22 @@ import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import { AuthComponent } from './components/auth/auth.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AuthService } from './services/auth.service';
-import { AllConferencesComponent } from './components/all-conferences/all-conferences.component';
+import {AuthComponent} from './components/auth/auth.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {AuthService} from './services/auth.service';
+import {AllConferencesComponent} from './components/all-conferences/all-conferences.component';
 import {NgbPaginationModule, NgbAlertModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { CardComponent } from './components/card/card.component';
-import { HeaderComponent } from './components/header/header.component';
+import {CardComponent} from './components/card/card.component';
+import {HeaderComponent} from './components/header/header.component';
 import {ConferenceService} from './services/conference.service';
-import { HomeComponent } from './components/home/home.component';
+import {HomeComponent} from './components/home/home.component';
 import {UserService} from "./services/user.service";
-import { ProgramCommitteeService } from './services/programCommittee.service';
-import { ConferenceComponent } from './components/conference/conference.component';
-import { PaperCardComponent } from './components/paper-card/paper-card.component';
+import {ProgramCommitteeService} from './services/programCommittee.service';
+import {ConferenceComponent} from './components/conference/conference.component';
+import {PaperCardComponent} from './components/paper-card/paper-card.component';
 import {PaperService} from "./services/paper.service";
+
 @NgModule({
   declarations: [
     AppComponent,
--- app/app-web/src/main/webapp/src/app/components/paper-card/paper-card.component.ts
+++ app/app-web/src/main/webapp/src/app/components/paper-card/paper-card.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Paper} from "../../models/paper.model";
 import {Phase} from "../../models/phase.enum";
+import {PaperAgree} from "../../models/paper.agree";
 
 @Component({
   selector: 'app-paper-card',
@@ -11,13 +12,30 @@ export class PaperCardComponent implements OnInit {
   @Input() conferencePhase: string;
   @Input() paper: Paper;
   Phases = Phase;
+  PAPER_AGREE = PaperAgree;
   // paper: Paper;
   reviewerInput: string;
+  selectedInterest: string;
+  selectedAgreement: Boolean;
+  showTextarea: Boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.selectedInterest = null;
+    this.selectedAgreement = false;
+    this.reviewerInput = null;
+    this.showTextarea = true;
   }
-  submitReview(){
-    console.log("Added review", this.reviewerInput);
+  submitReview(paperAgree: PaperAgree){
+    console.log("Added review", paperAgree);
+    this.selectedAgreement = true;
+    this.showTextarea = false;
+  }
+
+  addBidding(interestLevel: string) {
+    console.log(interestLevel);
+    // alert("Thank you for your interest!");
+    this.selectedInterest = interestLevel;
+    // call some method
   }
 }
